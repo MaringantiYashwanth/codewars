d66ed461ebfc6243fb141a32ff40da4b9eb30c38
MaringantiYashwanth <yashwanthmaringanti@gmail.com>
2026-03-14 23:33:00 +0530

    different solutions

--- seqToOne.js
+++ seqToOne.js
@@ -10,4 +10,24 @@ function seqToOne(n) {
     }
   }
   return arr;
+
+  // another way to solve
+  //   const arr2 = [];
+  //   while (n !== 1) {
+  //     if (n > 1) {
+  //       arr.push(n--);
+  //     } else {
+  //       arr.push(n++);
+  //     }
+  //   }
+  //   arr.push(1);
+  //   return arr;
+
+  // most optimal method
+  //   return [...Array(Math.abs(n - 1) + 1)].map((_, i) => (n < 1 ? n + i : n - i));
+  // const length = num > 1 ? num: 2 - num;
+  // const sequence = (_, i) => num + (num > 1 ? -i : i);
+  // return Array.from({length}, sequence);
+
+  // return Array.from({length: Math.abs(n - 1) + 1}, (_, i) => n + (n < 1? i: -i))
 }
